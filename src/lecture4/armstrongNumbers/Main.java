37793592b1e71daf3da14ece21e48f833014ecdb
vedasole <vedasole001@gmail.com>
2024-08-03 20:43:08 +0530

    added l4-armstrongNumbers problem and solution

---
+++ src/lecture4/armstrongNumbers/Main.java
@@ -0,0 +1,40 @@
+package lecture4.armstrongNumbers;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while(t-- > 0)
+        {
+            int N = Integer.parseInt(read.readLine());
+            if (Solution.isArmstrongNumber(N)) {
+                System.out.println(N + " is an Armstrong number.");
+            } else {
+                System.out.println(N + " is not an Armstrong number.");
+            }
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function template for Java
+class Solution {
+    public static boolean isArmstrongNumber(int n) {
+        int original = n;
+        int length = Integer.toString(n).length();
+        int sum=0;
+        for (int i = 0; i < length; i++) {
+            int digit = n%10;
+            sum += Math.pow(digit, length);
+            n = n/10;
+        }
+        return original == sum;
+    }
+}
---
+++ src/lecture4/armstrongNumbers/Readme.md
@@ -0,0 +1,28 @@
+# 	
+Armstrong Numbers
+Leetcode - https://leetcode.com/problems/armstrong-number
+
+
+Given an integer `N`, return true it is an Armstrong number otherwise return false.
+
+An Armstrong number is a number that is equal to the sum of its own digits each raised to the power of the number of digits.
+
+Example 1:
+
+    Input: N = 153
+    Output: True
+    Explanation: 13+53+33 = 1 + 125 + 27 = 153
+
+Example 2:
+
+    Input: N = 371
+    Output: True
+    Explanation: 33+53+13 = 27 + 343 + 1 = 371
+
+### Your Task:
+
+You don't need to read input or print anything. Your task is to complete the function isArmstrongNumber() which takes an integer N as input parameters and returns true it is an Armstrong number otherwise return false.
+
+Expected Time Complexity: O(log10N + 1)
+
+Expected Space Complexity: O(1)
